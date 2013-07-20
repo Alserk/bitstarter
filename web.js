1c0b6501e2e44d2e31546a3a618e9ebd22bba04c
Aleksandr Kondrik <exile1985@gmail.com>
2013-07-20 08:26:50 +0000

    add web

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var fs=request('fs');
+var buffer = fs.readFileSync("index.html","utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	var buffer = fs.readFileSync("index.html");
-	console.log(buffer);
-//	concole.log(buffer.toString('utf-8'))
-//	response.send(buffer.toString('utf-8'));
+//  	var buffer = fs.readFileSync("index.html");
+//	console.log(buffer);
+	concole.log(fs.readFileSync("index.html","utf-8").toString('utf-8'))
+	response.send(fs.readFileSync("index.html","utf-8").toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
